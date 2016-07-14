2b63a28401abab16f909c9f167b7ba1a339241ce
dburriss <devon@chimplab.co>
2016-07-14 14:33:43 +0200

    1st session progress update
    8 July result of craftsmanship workshop teaching TDD

---
+++ Readme.md
@@ -0,0 +1,43 @@
+﻿# Roman numerals kata
+
+The goal is to create a program that can convert Roman numerals to numeric numbers and also the other way around.
+How are you going to do this? *TDD* baby!
+
+[Dont like TDD?](DontLikeTDD.md)
+
+## Use cases
+
+As a game developer
+I want to be able to convert a number to a numeral
+So that I can label my game releases using Roman numerals
+Given I have started the converter
+When I enter $number
+Then $numeral is returned
+
+As a marketing manager
+I want customers to be able to convert numerals to numbers
+So that they can buy the latest version of the game
+Given I have started the converter
+When I enter $numeral
+Then $number is returned
+
+## Extra details about Roman numerals
+
+The Romans wrote their numbers using letters; 
+specifically the letters 
+'I' meaning '1', 
+'V' meaning '5', 
+'X' meaning '10', 
+'L' meaning '50', 
+'C' meaning '100', 
+'D' meaning '500', 
+and 'M' meaning '1000'. 
+
+There were certain rules that the numerals followed which should be observed.
+
+The symbols 'I', 'X', 'C', and 'M' can be repeated at most 3 times in a row. 
+The symbols 'V', 'L', and 'D' can never be repeated. 
+The '1' symbols ('I', 'X', and 'C') can only be subtracted from the 2 next highest values ('IV' and 'IX', 'XL' and 'XC', 'CD' and 'CM'). 
+Only one subtraction can be made per numeral 
+('XC' is allowed, 'XXC' is not). 
+The '5' symbols ('V', 'L', and 'D') can never be subtracted.
---
+++ Roman/ArabicToRomanNumberConverterTests.cs
@@ -0,0 +1,80 @@
+﻿using FluentAssertions;
+using Xunit;
+
+namespace Roman
+{
+    public class ArabicToRomanNumberConverterTests
+    {
+        private ArabicToRomanNumberConverter CreateSut() => 
+            new ArabicToRomanNumberConverter();
+
+        [Theory]
+        [InlineData(2, "II")]
+        [InlineData(3, "III")]
+        public void ShouldConvert_ToMultipleRomanCharacters(int number, string expected)
+        {
+            // arrange
+            ArabicToRomanNumberConverter sut = CreateSut();
+
+            // act
+            string result = sut.Convert(number);
+
+            // assert
+            result.Should().Be(expected);
+        }
+
+        [Theory]
+        [InlineData(4, "IV")]
+        public void Convert_Four_ToRomanEquivalent(int number, string expected)
+        {
+            // arrange
+            ArabicToRomanNumberConverter sut = CreateSut();
+
+            // act
+            string result = sut.Convert(number);
+
+            // assert
+            result.Should().Be(expected);
+        }
+
+        [Theory]
+        [InlineData(1, "I")]
+        [InlineData(5, "V")]
+        [InlineData(10, "X")]
+        [InlineData(50, "L")]
+        [InlineData(100, "C")]
+        [InlineData(500, "D")]
+        [InlineData(1000, "M")]
+        public void ShouldConvert_ToSingleRomanCharacter_When(int number, string expected)
+        {
+            // arrange
+            ArabicToRomanNumberConverter sut = CreateSut();
+
+            // act
+            string result = sut.Convert(number);
+
+            // assert
+            result.Should().Be(expected);
+        }
+    }
+
+    public class ArabicToRomanNumberConverter
+    {
+        public string Convert(int number)
+        {
+            switch (number)
+            {
+                case 1000: return "M";
+                case 500: return "D";
+                case 100: return "C";
+                case 50: return "L";
+                case 10: return "X";
+                case 5: return "V";
+                case 1: return "I";
+                case 4:return "IV";
+            }
+
+            return new string('I', number);
+        }
+    }
+}
